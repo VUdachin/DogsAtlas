d37a1242f2055796b899690d3e9cd9afa6d9ae33
Vladimir Udachin <51325302+VUdachin@users.noreply.github.com>
2020-10-09 12:48:45 +0300

    Add files via upload
    Starting project

---
+++ DogsAtlas/DogsAtlas/BreedInfo/BreedInfoWorker.swift
@@ -0,0 +1,23 @@
+//
+//  BreedInfoWorker.swift
+//  DogsAtlas
+//
+//  Created by Vladimir Udachin on 09.10.2020.
+//
+
+import Foundation
+
+protocol BreedInfoWorkingLogic {
+
+}
+
+final class BreedInfoWorker: BreedInfoWorkingLogic {
+  
+  // MARK: - Private Properties
+  
+  //
+
+  // MARK: - Working Logic
+  
+  //
+}
